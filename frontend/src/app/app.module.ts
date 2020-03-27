58afbf021ed246aef0577845dce2ef961cf3db4c
duilmarc <franco.chp@gmail.com>
2020-03-27 18:20:48 -0500

    Agregando componentes y servicios creados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TechnologyCardComponent } from './components/technology-card/technology-card.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SearchComponent } from './pages/search/search.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TechnologyCardComponent,
+    AboutComponent,
+    SearchComponent,
+    TechnologiesComponent,
+    TechnologyComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
